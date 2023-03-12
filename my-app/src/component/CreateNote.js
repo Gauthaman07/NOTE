d58c43ffd8b253f1da3974f93f299cded0627eff
Gautham <gauthaman101@gmail.com>
2023-03-12 20:20:24 +0530

    first

--- my-app/src/component/CreateNote.js
+++ my-app/src/component/CreateNote.js
@@ -2,38 +2,39 @@ import { useState } from "react";
 
 
 
-function CreateNote(props){
-const[note, setNote] = useState({
-    title:"",
-    content:""
-})
-const handleChange = (event)=>{
-    const {name, value} = event.target;
-    setNote({...note,
-    [name]:value})
+function CreateNote(props) {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    })
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setNote({
+            ...note,
+            [name]: value
+        })
 
-}
+    }
 
-const submitNote = (e)=>{
-e.preventDefault()
-props.onAdd(note)
-setNote({
-    title:"",
-    content:""
-})
-}
+    const submitNote = (e) => {
+        e.preventDefault()
+        props.onAdd(note)
+        setNote({
+            title: "",
+            content: ""
+        })
+    }
 
-return(
-    <div>
-        <h1>NOTEPAD</h1>
-<form>
-   
-    <input name="title" value={note.title} onChange={handleChange} placeholder="Enter title here"></input>
-    <textarea name="content" value={note.content} onChange={handleChange} placeholder="Enter content here"></textarea>
-    <button onClick={submitNote}>ADD</button>
-</form>
-</div>
-)
+    return (
+        <div >
+            <h1>NOTEPAD</h1>
+            <form>
+                <input name="title" value={note.title} onChange={handleChange} placeholder="Enter title here"></input>
+                <textarea name="content" value={note.content} onChange={handleChange} placeholder="Enter content here"></textarea>
+                <button onClick={submitNote}>ADD</button>
+            </form>
+        </div>
+    )
 }
 
 
